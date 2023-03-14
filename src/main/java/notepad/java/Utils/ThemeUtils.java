befa8bf1a4554f8cfb28a54520ac5c332c435eb4
SplashAni <106507600+SplashAni@users.noreply.github.com>
2023-03-14 21:24:18 +0200

    :cold_sweat: skillful

--- src/main/java/notepad/java/Utils/ThemeUtils.java
+++ src/main/java/notepad/java/Utils/ThemeUtils.java
@@ -1,13 +1,26 @@
 package notepad.java.Utils;
 
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
+
+
+import static java.io.File.separator;
+import static notepad.java.Utils.Utils.*;
+import static notepad.java.Utils.Utils.mainPath;
 
 public class ThemeUtils extends JFrame {
+
+    private String mainPath =  Utils.mainPath + separator + "custom-themes";
+
+    private String tempTheme = mainPath + separator;
+
+
+    private File themeFile = new File(mainPath);
+
     public static void Manager(JFrame window, JTextArea textArea) {
         textArea.setText("testing");
     }
@@ -15,7 +28,9 @@ public class ThemeUtils extends JFrame {
     public void themeCreator() {
         setTitle("Theme Creator");
         FlatMacLightLaf.install();
-        setLayout(new GridBagLayout()); // thanks bro code
+        createThemeConfig();
+        refresh();
+        setLayout(new GridBagLayout());
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
@@ -31,7 +46,6 @@ public class ThemeUtils extends JFrame {
         gbc.gridwidth = 1;
 
         JLabel l1 = new JLabel("Background");
-
         l1.setFont(new Font("Arial", Font.PLAIN, 18));
         gbc.gridx = 0;
         gbc.gridy = 1;
@@ -55,7 +69,7 @@ public class ThemeUtils extends JFrame {
         gbc.gridy = 2;
         add(fontButton, gbc);
 
-        JLabel l3 = new JLabel("Size");
+        JLabel l3 = new JLabel("Font Size");
         l3.setFont(new Font("Arial", Font.PLAIN, 18));
         gbc.gridx = 0;
         gbc.gridy = 3;
@@ -79,8 +93,151 @@ public class ThemeUtils extends JFrame {
         gbc.gridy = 4;
         add(iconButton, gbc);
 
-        setSize(300,320);
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JButton saveButton = new JButton("Save");
+        saveButton.setPreferredSize(new Dimension(100, 30));
+        buttonPanel.add(saveButton);
+        JButton close = new JButton("close");
+        close.setPreferredSize(new Dimension(100, 30));
+        buttonPanel.add(close);
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.PAGE_END;
+        add(buttonPanel, gbc);
+
+        colorButton.addActionListener(e -> chooseColour());
+        fontButton.addActionListener(e -> chooseFont());
+        sizeButton.addActionListener(e -> chooseFontSize());
+        iconButton.addActionListener(e -> chooseIcon());
+        saveButton.addActionListener(e -> saving());
+        close.addActionListener(e -> this.setVisible(false));
+
+        setResizable(false);
+        setSize(300, 400);
         setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void createThemeConfig() {
+        if (!themeFile.exists()) {
+            themeFile.mkdir();
+        }
+    }
+    private void refresh(){
+        File tempThemeFile = new File(mainPath,"temp.theme");
+        if(tempThemeFile.exists()){
+            tempThemeFile.delete();
+            tempThemeFile.mkdir();
+        }
+        else {
+            tempThemeFile.mkdir();
+        }
+    }
+
+    private void chooseColour() {
+        Color backgroundColor = JColorChooser.showDialog(null, "Choose a color", Color.LIGHT_GRAY);
+        if (backgroundColor != null) {
+            write(tempTheme + "", String.valueOf(backgroundColor), 1);
+        }
+    }
+
+    private void chooseFont() {
+        GraphicsEnvironment beepboop = GraphicsEnvironment.getLocalGraphicsEnvironment(); // all credits due to some random on stackoveflow for this
+        Font[] allFonts = beepboop.getAllFonts();
+        String[] fontNames = new String[allFonts.length];
+
+        for (int i = 0; i < allFonts.length; i++) {
+            fontNames[i] = allFonts[i].getFontName();
+        }
+
+        String selectedFont = (String) JOptionPane.showInputDialog(null, "Choose a font:", "Font", JOptionPane.QUESTION_MESSAGE, null, fontNames, fontNames[0]);
+
+        if (selectedFont != null) {
+            write(tempTheme, selectedFont, 2);
+        }
+    }
+
+    public void chooseFontSize() {
+        Integer[] ints = new Integer[16];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = i + 1;
+        }
+        int defaultOption = 6;
+        JComboBox<Integer> comboBox = new JComboBox<>(ints);
+        comboBox.setSelectedItem(defaultOption);
+        JOptionPane.showMessageDialog(null, comboBox, "Font size", JOptionPane.QUESTION_MESSAGE);
+        int selectedSize = (int) comboBox.getSelectedItem();
+        write(tempTheme, String.valueOf(selectedSize), 3);
+    }
+
+    private void chooseIcon() {
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Types", "jpg", "jpeg", "png");
+        fileChooser.setFileFilter(filter);
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File imgPath = fileChooser.getSelectedFile();
+            write(tempTheme, String.valueOf(imgPath), 4);
+        }
+    }
+
+    private void saving() {
+        Component[] components = getContentPane().getComponents();
+        for (Component c : components) {
+            c.setVisible(false);
+        }
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        JLabel[] labels = new JLabel[4];
+        labels[0] = new JLabel("Color: " + read(tempTheme ,"d", 1));
+        labels[1] = new JLabel("Font: " + read(tempTheme,2));
+        labels[2] = new JLabel("Font Size: " + read(tempTheme,3));
+        labels[3] = new JLabel("Icon: " + read(tempTheme,4));
+
+        Font font = new Font("Arial", Font.BOLD, 18);
+        for (JLabel label : labels) {
+            label.setFont(font);
+        }
+
+        c.insets = new Insets(0, 0, 10, 0);
+
+        c.gridx = 0;
+        c.gridy = 1;
+
+        add(labels[0], c);
+
+        c.gridy = 2;
+
+        add(labels[1], c);
+
+        c.gridy = 3;
+
+        add(labels[2], c);
+
+        c.gridy = 4;
+
+        add(labels[3], c);
+
+        JTextField iconTextField = new JTextField();
+
+        c.gridx = 0;
+        c.gridy = 5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        add(iconTextField, c);
+
+        JButton confirm = new JButton("Confirm");
+
+        c.gridx = 0;
+        c.gridy = 6;
+        c.fill = GridBagConstraints.NONE;
+        c.anchor = GridBagConstraints.SOUTH;
+        c.insets = new Insets(20, 0, 0, 0);
+
+        add(confirm, c);
+        setSize(300, 300);
         setVisible(true);
     }
 }
--- src/main/java/notepad/java/Utils/Utils.java
+++ src/main/java/notepad/java/Utils/Utils.java
@@ -36,26 +36,27 @@ public class Utils {
         }
         return true;
     }
-    public static void write(String path,String content){
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(mainPath + separator + path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            writer.write(content);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            writer.close();
+    public static void write(String path, String content, int line) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            String currentLine;
+            int lineNumber = 1;
+            while ((currentLine = reader.readLine()) != null) {
+                if (lineNumber == line) {
+                    writer.write(content);
+                } else {
+                    writer.write(currentLine);
+                }
+                writer.newLine();
+                lineNumber++;
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static String read(String path) {
+
+    public static String read(String path, int lineNum) {
         StringBuilder content = new StringBuilder();
         BufferedReader reader = null;
 
@@ -64,10 +65,12 @@ public class Utils {
             reader = new BufferedReader(new FileReader(file));
 
             String line;
+            int currentLineNum = 0;
             while ((line = reader.readLine()) != null) {
-                content.append(line);
-                if (reader.ready()) {
-                    content.append("\n");
+                currentLineNum++;
+                if (currentLineNum == lineNum) {
+                    content.append(line);
+                    break;
                 }
             }
         } catch (IOException e) {
@@ -85,6 +88,7 @@ public class Utils {
         return content.toString();
     }
 
+
     public static void init(){ // basically setup
         FlatDarkLaf.install();
         config(false);
@@ -92,49 +96,49 @@ public class Utils {
 
         // name
         String username = JOptionPane.showInputDialog("Whats your name?", "SplashAni");
-        write(configFile +"username.txt", username);
+        write(configFile +"username.txt", username,1);
 
         // window size
         String w = JOptionPane.showInputDialog(null, "Select the editors width", "800");
         int width = Integer.parseInt(w);
         String h = JOptionPane.showInputDialog(null, "Select the editors height", "600");
         int height = Integer.parseInt(h);
-        write(configPath + separator + "windowSize.txt",width + "," + height);
+        write(configPath + separator + "windowSize.txt",width + "," + height,1);
 
 
         //resizable
         int response = JOptionPane.showConfirmDialog(null, "Should the editors window be resizable", "Boolean", JOptionPane.YES_NO_OPTION);
         if (response == JOptionPane.YES_OPTION) {
-            write(configPath + separator + "resizeable.txt","true");
+            write(configPath + separator + "resizeable.txt","true",1);
         } else {
-            write(configPath + separator + "resizeable.txt", "false");
+            write(configPath + separator + "resizeable.txt", "false",1);
         }
 
         //theme
         String[] themes = {"Dark", "Light", "Custom"};
         String theme = (String) JOptionPane.showInputDialog(null, "Select an theme:", "Theme", JOptionPane.QUESTION_MESSAGE, null, themes, themes[0]);
         if(theme != null) { // i love java symbols :yum:
-            write( "theme.txt", theme);
-            write(configPath,"800,600");
+            write( "theme.txt", theme,1);
+            write(configPath,"800,600",1);
             new Notepad();
         } else {
             config(true);
         }
     }
     public static String theme(){
-        String choosenTheme = read(configFile+ separator + "theme.txt");
+        String choosenTheme = read(configFile+ separator + "theme.txt",1);
         return choosenTheme;
     }
     public static void setWindowSize(JFrame window){
         File dir = new File(configPath + separator + "windowSize.txt");
-        String open = read(String.valueOf(dir));
+        String open = read(String.valueOf(dir),1);
         String[] splitValues = open.split(",");
         int width = Integer.parseInt(splitValues[0]);
         int height = Integer.parseInt(splitValues[1]);
         window.setSize(width, height);
     }
     public static void resizeable(JFrame window){
-        switch (read(configPath + separator + "resizeable.txt")){
+        switch (read(configPath + separator + "resizeable.txt",1)){
             case "true":
                 window.setResizable(true);
             case "false":
